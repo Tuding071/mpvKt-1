cc61829be41dff04d4371ad055d69ee042e66fbc
Tuding071 <davidpenaranda071@gmail.com>
2025-10-14 17:54:17 +0800

    Update PlayerControls.kt

--- app/src/main/java/live/mehiz/mpvkt/ui/player/controls/PlayerControls.kt
+++ app/src/main/java/live/mehiz/mpvkt/ui/player/controls/PlayerControls.kt
@@ -134,7 +134,7 @@ fun PlayerControls(
               Pair(.7f, Color.Transparent),
               Pair(1f, Color.Black),
             ),
-            alpha = transparentOverlay,
+            alpha = 0f,
           )
           .padding(horizontal = MaterialTheme.spacing.medium),
       ) {
